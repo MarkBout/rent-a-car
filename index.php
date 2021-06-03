8b9278dc11b2ba7160f99d73fdab958e7a8e0e2a
Mark Bout <mark@conduction.nl>
2021-06-03 09:34:04 +0200

    hoofdpagina tekst

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ $afbeeldingen = $database->getObject($connection,'auto',array('afbeelding'));
             ?>
             <h1 class="text-center text-white mb-3">Welkom bij Rent a Car</h1>
             <div class="row w-50 position-relative" style="left: 25%">
-                <p class="text-white " style="text-align: justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sem lorem, pellentesque eu efficitur et, lobortis nec quam. Proin egestas, augue quis convallis tristique, elit massa ullamcorper ante, nec tincidunt magna felis vitae dolor. Suspendisse venenatis eros quis elit gravida dictum. Phasellus pellentesque viverra massa, non fermentum leo sagittis id. Sed id sodales nulla, id pulvinar ligula.</p>
+                <p class="text-white ">Rent a car is een autoverhuurbedrijf waar we er alles aan om jou de vrijheid te geven om meer te ontdekken. We verzetten bergen om je van start tot finish een naadloze gebruikservaring te bieden en de juiste huurauto voor je te vinden.<br><br>Een huurauto geeft je vrijheid: wij helpen je om de juiste auto te vinden voor een geweldige prijs. Maar dat is nog niet alles. Wij willen het veel makkelijker maken om een auto te huren daarom hebben wij op deze site het mogelijk gemaakt om van alles te beheren met ons nieuwe acountsysteem.<br><br>Kijk gerust rond op onze vernieuwde site!</p>
                 <div class="btn-group" role="group" aria-label="index buttons">
                     <a type="button" href="autoOverzicht.php" class="text-white btn btn-primary rounded-pill me-1">Huur een auto</a>
                     <a type="button" href="contact.php" class="text-white btn btn-primary rounded-pill ms-1">Kom in contact</a>
