90fbb88c48c5db4c4b50c6e64e0809408bbe59ce
Mark Bout <mbout@roc-dev.com>
2021-06-05 01:38:41 +0200

    navbar rework

--- navbar.php
+++ navbar.php
@@ -41,56 +41,58 @@ $connection = $database->setConn('localhost', 'root',null,'rent-a-car');
     </style>
 </head>
 <header>
-    <nav class="navbar navbar-dark primary-colour navbar-expand-md bg-faded justify-content-center fixed-top">
-        <div class="navbar-collapse collapse w-25 order-1 order-md-0 dual-collapse2">
-            <a class="navbar-brand border-0 " href="index.php">
-                <img src="images/logo.png" height="65" alt="LOGO">
-            </a>
-            <h3 class="text-white"> Rent a car | Autoverhuur </h3>
-        </div>
-        <div class="navbar-collapse collapse w-50" id="collapsingNavbar3">
-            <ul class="nav navbar-nav ml-auto w-75 justify-content-end">
-                <?php if(isset($_SESSION['gebruiker'])): ?>
-                    <li class="nav-item dropdown rounded-pill mr-2" style="background-color: #1361C2;">
-                        <a class="nav-link dropdown-toggle text-white text-center" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <?php echo $_SESSION['gebruiker']['voornaam'].' '.$_SESSION['gebruiker']['tussenvoegsel'].' '.$_SESSION['gebruiker']['achternaam'].' '. $utilities->getRole($_SESSION['gebruiker']['rol']); ?>
-                        </a>
-                        <ul class="dropdown-menu" style="background-color: #1361C2;" aria-labelledby="navbarDarkDropdownMenuLink">
-                            <li><a class="dropdown-item text-white" href="Profiel.php">Mijn Profiel</a></li>
-                            <?php if(isset($_SESSION['gebruiker']) && $_SESSION['gebruiker']['rol'] == 1): ?>
-                            <li><a class="dropdown-item text-white" href="medewerkers.php">Medewerkers pagina</a></li>
-                            <?php endif;?>
-                            <li><a class="dropdown-item text-white" href="winkelwagen.php">Winkelwagen</a></li>
-                            <li><a class="dropdown-item text-white" href="logout.php">Uitloggen</a></li>
-                        </ul>
-                    </li>
-                <?php else: ?>
-                    <li class="nav-item">
-                        <button type="button" class="btn btn-primary rounded-pill btn-lg"><a
-                                    class="text-white text-decoration-none" data-bs-toggle="modal" data-bs-target="#registerModal">Registreren</a></button>
-                    </li>
+
+    <nav class="navbar primary-colour">
+        <div class="container-fluid">
+            <div class="d-flex align-items-center">
+                <div>
+                    <img class="img-fluid" src="images/logo.png" alt="Card image cap">
+                </div>
+                <h2 class="text-white col p-2 m-0">Rent a Car | Autoverhuur</h2>
+            </div>
+            <div class="d-flex">
+                <ul class="list-group list-group-horizontal-lg" style="list-style: none">
+                    <?php if(isset($_SESSION['gebruiker'])): ?>
+                        <li class="nav-item dropdown rounded-pill mr-2" style="background-color: #1361C2;">
+                            <a class="nav-link dropdown-toggle text-white text-center" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <?php echo $_SESSION['gebruiker']['voornaam'].' '.$_SESSION['gebruiker']['tussenvoegsel'].' '.$_SESSION['gebruiker']['achternaam'].' '. $utilities->getRole($_SESSION['gebruiker']['rol']); ?>
+                            </a>
+                            <ul class="dropdown-menu" style="background-color: #1361C2;" aria-labelledby="navbarDarkDropdownMenuLink">
+                                <li><a class="dropdown-item text-white" href="Profiel.php">Mijn Profiel</a></li>
+                                <?php if(isset($_SESSION['gebruiker']) && $_SESSION['gebruiker']['rol'] == 1): ?>
+                                    <li><a class="dropdown-item text-white" href="medewerkers.php">Medewerkers pagina</a></li>
+                                <?php endif;?>
+                                <li><a class="dropdown-item text-white" href="winkelwagen.php">Winkelwagen</a></li>
+                                <li><a class="dropdown-item text-white" href="logout.php">Uitloggen</a></li>
+                            </ul>
+                        </li>
+                    <?php else: ?>
+                        <li class="nav-item">
+                            <button type="button" class="btn btn-primary rounded-pill btn-lg"><a
+                                        class="text-white text-decoration-none" data-bs-toggle="modal" data-bs-target="#registerModal">Registreren</a></button>
+                        </li>
+                        <li class="nav-item mr-2">
+                            <button type="button" class="btn btn-primary ms-2 rounded-pill btn-lg"><a
+                                        class="text-white text-decoration-none" data-bs-toggle="modal" data-bs-target="#loginModal">inloggen</a></button>
+                        </li>
+                    <?php endif;?>
                     <li class="nav-item mr-2">
-                        <button type="button" class="btn btn-primary ms-2 rounded-pill btn-lg"><a
-                                    class="text-white text-decoration-none" data-bs-toggle="modal" data-bs-target="#loginModal">inloggen</a></button>
+                        <button type="button" class="w-100 btn btn-primary rounded-pill btn-lg">
+                            <a class="text-white text-decoration-none" href="index.php">Home</a>
+                        </button>
                     </li>
-                <?php endif;?>
-                <li class="nav-item mr-2">
-                    <button type="button" class="w-100 btn btn-primary rounded-pill btn-lg">
-                        <a class="text-white text-decoration-none" href="index.php">Home</a>
-                    </button>
-                </li>
-                <li class="nav-item">
-                    <button type="button" class="w-100 btn btn-primary rounded-pill btn-lg">
-                        <a class="text-white text-decoration-none" href="contact.php">Contact</a>
-                    </button>
-                </li>
-                <li class="ms-2 me-2 nav-item">
-                    <button type="button" class="w-100 btn btn-primary rounded-pill btn-lg">
-                        <a class="text-white text-decoration-none" href="autoOverzicht.php">Onze auto's</a>
-                    </button>
-                </li>
-            </ul>
+                    <li class="nav-item">
+                        <button type="button" class="w-100 btn btn-primary rounded-pill btn-lg">
+                            <a class="text-white text-decoration-none" href="contact.php">Contact</a>
+                        </button>
+                    </li>
+                    <li class="ms-2 me-2 nav-item">
+                        <button type="button" class="w-100 btn btn-primary rounded-pill btn-lg">
+                            <a class="text-white text-decoration-none" href="autoOverzicht.php">Onze auto's</a>
+                        </button>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </nav>
-</header>
+    </nav></header>
 <script src="lib/bootstrap-5/js/bootstrap.min.js"></script>
